1b94bf728ba9a1836ee58f91fbe2768bfba8eeee
Kumarjeet Das <kumarjeetdas58@gmail.com>
2022-12-28 14:10:46 +0530

    first commit

---
+++ .aws-sam/build/ListTodosFunction/createTodo.js
@@ -0,0 +1,51 @@
+const AWS = require("aws-sdk");
+
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require("uuid");
+/**
+ * 
+ * Must first run npm install uuid inside the handler folder 
+ * since that's where package.json was created!!
+ */
+exports.createTodo = async (event, context) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const params = {
+    TableName: TODO_TABLE,
+    Item: {
+      id: uuid.v1(),
+      todo: data.todo,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  if (typeof data.todo !== "string") {
+    console.error("Validation Failed");
+    return;
+  }
+
+  try {
+    body = await dynamoDb.put((params)).promise();
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+    console.log(err);
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+};
---
+++ handler/deleteTodo.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+let body;
+let statusCode = 200;
+const headers = {
+  "Content-Type": "application/json",
+};
+
+exports.deleteTodo = async (event, context) => {
+  const params = {
+    TableName: TODO_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+  await dynamoDb.delete(params).promise();
+  body = `Deleted todo ${event.pathParameters.id}`;
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
+
